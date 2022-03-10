5557549188d1a82437a3eae223d20dff334b44b2
Narmy <jo25bu@gmail.com>
2022-03-10 22:21:22 +0100

    🐛 Add auto-command syncing

--- utils/bot.py
+++ utils/bot.py
@@ -4,14 +4,20 @@ import logging
 
 
 class Langohrmarodeur(commands.Bot):
-    def __init__(self, command_prefix: str, intents: discord.Intents):
-        super().__init__(command_prefix, intents=intents)
+    def __init__(self, command_prefix: str, activity: discord.BaseActivity, intents: discord.Intents):
+        super().__init__(command_prefix, activity=activity, intents=intents)
 
         self.on_ready_has_run: bool = False
 
         self.logger = logging.getLogger("main")
         self.logger.setLevel(logging.DEBUG)
         self.logger.addHandler(logging.StreamHandler())
+
+        self.commands_guild: discord.Object = discord.Object(900793586898067476)
+
+    async def on_connect(self):
+        commands = await self.tree.sync(guild=self.commands_guild)
+        print([n.name for n in commands])
 
     async def on_ready(self):
         if self.on_ready_has_run:
